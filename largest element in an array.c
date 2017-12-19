8a72f7800afe186880809cc8cbbb1200a7d3f04d
preethiniranjana <32997412+preethiniranjana@users.noreply.github.com>
2017-12-19 11:00:44 +0530

    Update largest element in an array.c

--- largest element in an array.c
+++ largest element in an array.c
@@ -2,17 +2,17 @@
 int max(int[],int);
 int main(void) 
 {
-int a[]={10,45,60,32,67,88,99,10};
-int size=8,out;
-out=max(a,size);
-printf("%d",out);
+      int a[]={10,45,60,32,67,88,99,10};
+      int size=8,out;
+      out=max(a,size);
+      printf("%d",out);
 }
 int max(int a[],int size)
 {
-	int i,t=a[0];
-	for(i=0;i<size;i++)
+      int i,t=a[0];
+      for(i=0;i<size;i++)
 	{
-		if(a[i]>t)
+	    if(a[i]>t)
 		{
 		t=a[i];
 		}
